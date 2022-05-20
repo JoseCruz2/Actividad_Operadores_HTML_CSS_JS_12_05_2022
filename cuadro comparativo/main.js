f99ca2dc09fe00f9c9f968c526b84b28b228ea57
JoseCruz2 <josefernandocruzcarrillo22@gmail.com>
2022-05-19 19:57:23 -0500

    Cambios

--- cuadro comparativo/main.js
+++ cuadro comparativo/main.js
@@ -1,6 +1,11 @@
+let A = (prompt("Ingrese el valor de la variable A\n false\n true") == "true") ? true : false;
+let B = (prompt("Ingrese el valor de la variable B\n false\n true") == "true") ? true : false;
 
-let carnet = true;
-let certificado = true;
 
-console.log(carnet && certificado);
-
+document.querySelector("#operadorAND").insertAdjacentHTML("beforeend", `
+    <tr class="resultado">
+        <td>${A}</td>
+        <td>${B}</td>
+        <td>${A && B}</td>
+    </tr>
+`);
